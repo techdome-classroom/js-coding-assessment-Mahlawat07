ddcb5aa5ee3e873c0ec753557930e6001bda791a
Sagar <sagarmahlawat121@gmail.com>
2024-11-11 14:49:22 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,7 +1,32 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
-    
+    function matchHelper(mIndex, pIndex) {
+   
+      if (mIndex === message.length && pIndex === pattern.length) {
+        return true;
+      }
+  
+      
+      if (pIndex === pattern.length) return false;
+  
+      
+      if (pattern[pIndex] === '*') {
+        
+        return matchHelper(mIndex, pIndex + 1) || (mIndex < message.length && matchHelper(mIndex + 1, pIndex));
+      }
+  
+      
+      if (pattern[pIndex] === '?') {
+        return mIndex < message.length && matchHelper(mIndex + 1, pIndex + 1);
+      }
+  
+      if (mIndex < message.length && message[mIndex] === pattern[pIndex]) {
+        return matchHelper(mIndex + 1, pIndex + 1);
+      }
+  
+      return false;
+    }
 
   };
   
