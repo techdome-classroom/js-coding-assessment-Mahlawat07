ba16e96c350144c2d9dae7c8813e3ea065c54c99
Sagar <sagarmahlawat121@gmail.com>
2024-11-11 14:58:19 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -2,6 +2,36 @@ const getTotalIsles = function (grid) {
 
 
   // write your code here
+  if (!grid || grid.length === 0) return 0;
+
+  let count = 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
+
+  function dfs(row, col) {
+    if (row < 0 || col < 0 || row >= rows || col >= cols || grid[row][col] === 'W') {
+      return;
+    }
+
+    grid[row][col] = 'W';
+
+    
+    dfs(row + 1, col); 
+    dfs(row - 1, col); 
+    dfs(row, col + 1); 
+    dfs(row, col - 1); 
+  }
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      if (grid[row][col] === 'L') { 
+        count++;
+        dfs(row, col); 
+      }
+    }
+  }
+
+  return count;
   
 
 };
