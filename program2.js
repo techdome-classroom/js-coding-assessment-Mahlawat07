45c68fb6ee14589fe402886c7ce7aee2838a0c51
Sagar <sagarmahlawat121@gmail.com>
2024-11-11 14:28:57 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,36 +1,7 @@
 const decodeTheRing = function (s, p) {
 
     // write your code here
-    function match(s, p) {
-      let i = 0;
-      let j = 0;
-      let starIndex = -1;
-      let matchIndex = -1;
 
-      while (i < s.length) {
-        if (j < p.length && (p[j] === '?' || p[j] === s[i])) {
-          i++;
-          j++;
-        }
-      
-        else if (starIndex !== -1) {
-          j = starIndex + 1;
-          matchIndex++;
-          i = matchIndex;
-        }
-
-        else {
-          return false;
-        }
-      }
-
-      while (j < p.length && p[j] === '*') {
-        j++;
-      }
-
-      return j === p.length;
-    }
-  return match(s, p);
-};
+  };
   
-module.exports = decodeTheRing;
+  module.exports = decodeTheRing;
